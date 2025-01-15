bea6f2d20a67255e3d465938e1c051369c517754
techocea <dunsfordbright@gmail.com>
2025-01-16 00:46:51 +0530

    added contact form submit

--- frontend/src/components/ContactForm.jsx
+++ frontend/src/components/ContactForm.jsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-  Form,
-  Input,
-  Select,
-  SelectItem,
-  Checkbox,
-  Button,
-} from "@nextui-org/react";
+import { Form, Input, Textarea, Button } from "@nextui-org/react";
 
 const ContactForm = () => {
   const [data, setData] = useState();
@@ -16,52 +9,68 @@ const ContactForm = () => {
     console.log(data);
   };
 
-  // const handleInputChange = (e)=>{
-  //   const {...setData, [name]:value} = e.target;
-  // }
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({
+      ...data,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="px-4 py-20 sm:p-6 lg:py-10 h-full">
       <div className="flex flex-col items-center justify-center">
         <h1 className="font-semibold uppercase leading-9 text-lg">
           Contact Section
         </h1>
-        <Form className="max-w-sm w-full justify-center items-center gap-y-10 z-[-10]">
-          <div className="flex flex-col gap-6 w-full">
-            <Input
-              label="Name"
-              labelPlacement="outside"
-              name="name"
-              placeholder="Enter your name"
-              className="font-medium"
-            />
+        <Form
+          onSubmit={handleSubmit}
+          className="w-full max-w-xs flex flex-col gap-4 z-[-20]"
+          validationBehavior="native"
+        >
+          <Input
+            isRequired
+            errorMessage="Please enter your name"
+            label="Name"
+            labelPlacement="outside"
+            name="name"
+            placeholder="Enter your name"
+            type="text"
+            onChange={handleInputChange}
+          />
+          <Input
+            isRequired
+            errorMessage="Please enter your contact number"
+            label="Contact"
+            labelPlacement="outside"
+            name="contact"
+            placeholder="Enter your contact number"
+            type="text"
+            onChange={handleInputChange}
+          />
+          <Input
+            isRequired
+            errorMessage="Please enter a valid email"
+            label="Email"
+            labelPlacement="outside"
+            name="email"
+            placeholder="Enter your email"
+            type="email"
+            onChange={handleInputChange}
+          />
+          <Textarea
+            isRequired
+            errorMessage="Please enter a your message"
+            className="max-w-xs"
+            label="Message"
+            name="message"
+            labelPlacement="outside"
+            placeholder="Enter your message"
+            onChange={handleInputChange}
+          />
 
-            <Input
-              label="Email"
-              labelPlacement="outside"
-              name="email"
-              placeholder="Enter your email"
-              type="email"
-              className="font-medium"
-            />
-            <Input
-              label="Contact"
-              labelPlacement="outside"
-              name="email"
-              placeholder="Enter your contact number"
-              type="text"
-              className="font-medium"
-            />
-            <Input
-              label="Message"
-              labelPlacement="outside"
-              name="email"
-              placeholder="Enter your Mesage"
-              type="email"
-              className="font-medium"
-            />
-          </div>
-          <Button color="primary" className="rounded-lg font-semibold">
-            SUBMIT
+          <Button color="primary" type="submit" className="w-full">
+            Submit
           </Button>
         </Form>
       </div>
