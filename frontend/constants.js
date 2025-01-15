0cacf74292cf481fddbd5f395e6299e796278c3e
techocea <dunsfordbright@gmail.com>
2025-01-16 00:25:17 +0530

    added faq

--- frontend/constants.js
+++ frontend/constants.js
@@ -7,7 +7,10 @@ export const NavItems = [
     title: "Busease",
     url: "/busease",
   },
-
+  {
+    title: "FAQ",
+    url: "/faq",
+  },
   {
     title: "Lost & Found",
     url: "/lost-n-found",
@@ -15,5 +18,61 @@ export const NavItems = [
   {
     title: "About",
     url: "/about",
+  },
+];
+
+export const questions = [
+  {
+    id: 1,
+    question: "How can I reserve a seat in advance?",
+    answer:
+      "Simply select your boarding location and destination. Thereafter check availability and select a preferred bus and mark your preferred seat.",
+  },
+  {
+    id: 2,
+    question: "Can I book the seat I want?",
+    answer:
+      "Yes. The seat diagram on the web is the actual seating arrangement in the bus, and you will be allocated the seat you selected.",
+  },
+  {
+    id: 3,
+    question: "Can I reserve a seat from any bus operating in Sri Lanka?",
+    answer:
+      "No. Only buses operated by Sri Lanka Transport Board are available for booking. Furthermore, you can only reserve highway buses.",
+  },
+  {
+    id: 4,
+    question: "How do I obtain the ticket?",
+    answer:
+      "An e-ticket will be sent to your email. You can show either the digital or printed ticket to board the bus.",
+  },
+  {
+    id: 5,
+    question: "Can I book a seat for part of the journey?",
+    answer:
+      "Yes, you can, but you will be charged for the full journey of the bus.",
+  },
+  {
+    id: 6,
+    question: "Can I give my ticket to another person?",
+    answer: "Yes. You can hand over the ticket to another person.",
+  },
+  {
+    id: 7,
+    question: "What happens if I miss the bus?",
+    answer:
+      "Unfortunately, if you miss the bus, the ticket will be considered invalid, and no refunds will be issued.",
+  },
+  {
+    id: 8,
+    question: "Can I cancel my reservation?",
+    answer:
+      "Yes, you can cancel your reservation up to 24 hours before the departure time. A cancellation fee may apply.",
+  },
+  {
+    id: 9,
+    question: "What should I do if my e-ticket doesn’t arrive?",
+    answer:
+      "If your e-ticket doesn’t arrive within 30 minutes of booking, contact our support team immediately for assistance.",
   },
 ];
